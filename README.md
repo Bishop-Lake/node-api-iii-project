28b4a422ac062c8e17f2013b4630fc9e9c455548
Bishop <lake.d.bishop@gmail.com>
2019-09-04 16:45:31 -0500

    validateUser complete

--- README.md
+++ README.md
@@ -86,3 +86,13 @@ The _Database Schemas_ for the `users` and `posts` resources are:
 
 We have provided test data for the resources.
 
+## Stretch Goals
+
+- Add the Post Router
+  - Implement all endpoints and middleware within `posts/postRouter.js`
+
+- Create a React App
+  - Use `create-react-app` to create an application inside the root folder, name it `client`.
+  - From the React application connect to the `/api/users` endpoint in the API and show the list of users.
+  - Add functionality to show the details of a user, including their posts, when clicking a user name in the list. Use React Router to navigate to a `/users/:id` route to show the user details.
+  - Add styling!
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const db = require('./userDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    
+router.post('/', validateUser, (req, res) => {
+    res.send('hello')
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,7 +12,13 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-    res.send(`this indeed works @ ${req.url}`)
+    db.get()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(() => {
+            res.status(500).json({Error: "Internal server error while trying to request users"})
+        })
 });
 
 router.get('/:id', (req, res) => {
@@ -37,7 +44,12 @@ function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
-
+    if (Object.keys(req.body).length == 0) {
+        res.status(400).json({ message: "missing user data" })
+    } else if (req.body.name == null) {
+        res.status(400).json({ message: "missing required name field" })
+    }
+    next()
 };
 
 function validatePost(req, res, next) {
